b419e409abdb53789614a737e047f31c57897d1f
HeyImRige <Robert.aron.tx@verizon.net>
2017-05-03 16:41:50 -0500

    unget char tests

--- src/JUnit4Printtokens2/TokenTests.java
+++ src/JUnit4Printtokens2/TokenTests.java
@@ -11,7 +11,8 @@ import java.io.InputStreamReader;
 import org.junit.Test;
 
 public class TokenTests {
-	
+	static String defaultFile = "./TestingFiles/Default_Test_File.txt";
+	static String noTextFile = "./TestingFiles/Test_File_No_Text.txt";
 	
 	/*
 	 * I'm not sure how to test two Buffered Readers with STD input
@@ -37,16 +38,11 @@ public class TokenTests {
 		Printtokens2 myTokenTest = new Printtokens2();
 		FileReader fr;
 		try {
-			fr = new FileReader("./TestingFiles/Default_Test_File.txt");
+			fr = new FileReader(defaultFile);
 			BufferedReader outputExpected = new BufferedReader(fr);
-			BufferedReader outputActual = myTokenTest.open_character_stream("./TestingFiles/Default_Test_File.txt");
-			try {
-				assertEquals(outputExpected.readLine(),outputActual.readLine());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} catch (FileNotFoundException e) {
+			BufferedReader outputActual = myTokenTest.open_character_stream(defaultFile);
+			assertEquals(outputExpected.readLine(),outputActual.readLine());
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -65,8 +61,86 @@ public class TokenTests {
 		assertEquals(null,outputActual);
 	}
 	
-
+	@Test
+	public void test_get_char_no_error(){
+		FileReader fr;
+		Printtokens2 myTokenTest = new Printtokens2();
+		
+		try {
+			fr = new FileReader(defaultFile);
+			BufferedReader myBr = new BufferedReader(fr);
+			myBr.mark(4);
+			int expectedOutput = myBr.read();
+			myBr.reset();
+			int actualOutput = myTokenTest.get_char(myBr);
+			assertEquals(expectedOutput,actualOutput);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+		
+		
+	@Test
+	public void test_get_char_error(){
+		Printtokens2 myTokenTest = new Printtokens2();
+		FileReader fr;
+		try {
+			fr = new FileReader(noTextFile);
+			BufferedReader brokenReader = new BufferedReader(fr);
+			fr.close();
+			int actualOutput = myTokenTest.get_char(brokenReader);
+			int expectedOutput = 0;
+			assertEquals(expectedOutput,actualOutput);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	
-
+	@Test
+	public void test_unget_char_io_succsess(){
+		Printtokens2 myTokenTest = new Printtokens2();
+		FileReader fr;
+		try {
+			fr = new FileReader(defaultFile);
+			BufferedReader myBr = new BufferedReader(fr);
+			myBr.mark(4);
+			int actualOutput = myTokenTest.unget_char(0, myBr);
+			int expectedOutput = 0;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+	
+	@Test
+	public void test_unget_char_io_error(){
+		Printtokens2 myTokenTest = new Printtokens2();
+		FileReader fr;
+		try {
+			fr = new FileReader(defaultFile);
+			BufferedReader myBr = new BufferedReader(fr);
+			int actualOutput = myTokenTest.unget_char(0, myBr);
+			int expectedOutput = 0;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+		
+		
+		
 }
+	
+
+	
+	
+
